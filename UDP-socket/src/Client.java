77acf337fe43f9469a7dbdd3b332c35c5b36d55c
Krishna <krishna@pop-os.localdomain>
2023-05-15 11:19:09 +0545

    UDP socket programming

---
+++ UDP-socket/src/Client.java
@@ -0,0 +1,21 @@
+import java.awt.image.DataBuffer;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Client");
+        DatagramSocket socket = new DatagramSocket(12345);
+        byte[] data = new byte[1024];
+        DatagramPacket packet = new DatagramPacket(data,data.length);
+
+
+        //receive the message
+        socket.receive(packet);
+        String message = new String(packet.getData(),0,packet.getLength());
+        System.out.println(message);
+        socket.close();
+    }
+}
---
+++ UDP-socket/src/Server.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        DatagramSocket socket = new DatagramSocket();
+        String message = "Hello World!";
+        InetAddress address = InetAddress.getByName("localhost");
+        DatagramPacket packet = new DatagramPacket(message.getBytes(),message.length(),address,12345);
+
+        //sending message
+        socket.send(packet);
+        socket.close();
+    }
+}
